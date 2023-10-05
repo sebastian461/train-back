148d938357a3b01e5d24d05bf97ae4a2454f180e
sebastian461 <sebastiano-461@hotmail.com>
2023-10-05 18:49:01 -0500

    Country relations
    Creation of the relationship between countries and cities

--- app/Filament/Resources/CountryResource.php
+++ app/Filament/Resources/CountryResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CountryResource\Pages;
 use App\Filament\Resources\CountryResource\RelationManagers;
+use App\Filament\Resources\CountryResource\RelationManagers\CityRelationManager;
 use App\Models\Country;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -66,7 +67,7 @@ class CountryResource extends Resource
   public static function getRelations(): array
   {
     return [
-      //
+      CityRelationManager::class,
     ];
   }
 
