bec4a5a1bc459858098277bc04e57bab41ac3b72
sebastian461 <sebastiano-461@hotmail.com>
2023-10-06 17:55:06 -0500

    AuthController
    Creation of login and logout functions as well as registration

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+/* AuthController */
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+/* Middleware */
+Route::middleware(['auth:sanctum'])->group(function () {
+  /* AuthController */
+  Route::post('logout', [AuthController::class, 'logout']);
 });
